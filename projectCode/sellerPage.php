7dfbcc3879daa567d5c7bd2a6f0d35a0f8a75957
=JoshT934 <=>
2025-04-04 16:45:19 +0100

    modified:   projectCode/sellerPage.php

--- projectCode/sellerPage.php
+++ projectCode/sellerPage.php
@@ -7,7 +7,42 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.html");
     exit();
 }
+$servername = "sci-project.lboro.ac.uk"; 
+$dbUsername = "295group6"; 
+$dbPassword = "wHiuTatMrdizq3JfNeAH"; 
+$dbName = "295group6"; 
+// Connect to MySQL database 
+$conn = new mysqli($servername, $dbUsername, $dbPassword, $dbName);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-// Welcome message
-echo "Welcome, " . $_SESSION['username'] . "! You are logged in as a seller.";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+    $listingName = $_POST('listing-name')
+    $listingDepartment = $_POST('department')
+    $listingDescription = $_POST('description')
+    $listingPrice = $_POST('price')
+    $listingPostage = $_POST('postage-fee')
+    $listingDeadline = strtotime($_POST('deadline'))
+    $listingStart = time()
+    $listingId = password_hash($listingName +$listingStart, PASSWORD_DEFAULT)
+    $uderId = $_SESSION['user_id']
+
+
+    $stmt = $conn->prepare("INSERT INTO iBayListings (itemId, userId, title, category, description, price, postage,start,finish) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?)");
+    $stmt->bind_param("sssssssss", $listingId, $uderId, $listingName, $listingDepartment, $listingDescription,  $listingPrice, $listingPostage, $listingStart, $listingDeadline);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->close();
+    
+     
+
+
+    
+
+$conn->close();
+
 ?>

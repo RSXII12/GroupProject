5d6882bbceca49a3c8e9bc1c894a3ef818b00a15
JoshT934 <joshua.trow.934@gmail.com>
2025-04-04 15:41:01 +0100

    modified:   projectCode/sellerPage.php

--- projectCode/sellerPage.php
+++ projectCode/sellerPage.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+$servername = "sci-project.lboro.ac.uk"; 
+$dbUsername = "295group6"; 
+$dbPassword = "wHiuTatMrdizq3JfNeAH"; 
+$dbName = "295group6"; 
 
 // Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
@@ -9,5 +13,24 @@ if (!isset($_SESSION['user_id'])) {
 }
 
 // Welcome message
-echo "Welcome, " . $_SESSION['username'] . "! You are logged in as a seller.";
+$conn = new mysqli($servername, $dbUsername, $dbPassword, $dbName);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $listingSellerId = $_SESSION['user_id'];
+    $listingName = $_POST['name'];
+    $listingDepartment = $_POST['department'];
+    $listingDescription = $_POST['listingDescription'];
+    $listingPrice = $_POST['price'];
+    $listingPostage = $_POST['postage-fee'];
+    $listingDeadline = $_POST['deadline'];
+    $listingPhoto1 = $_FILES['photo-input']['name'][0];
+    $listingId = password_hash($listingName, 'sha256');
+    if(!empty($_FILES['photo-input']['name'][1])) {
+        $listingPhoto2 = $_FILES['photo-input']['name'][1];
+    } else {
+        $listingPhoto2 = NULL;
+    }
+}
 ?>

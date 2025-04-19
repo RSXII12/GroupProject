8c9e21da03ac0b8eec0e06afe04b57b8d243c4f4
RSXII12 <rogerarr06@gmail.com>
2025-04-19 14:15:04 +0100

    Create db.php

---
+++ projectCode/db.php
@@ -0,0 +1,16 @@
+<?php
+// db.php
+
+$servername = "sci-project.lboro.ac.uk";
+$username = "295group6";
+$password = "wHiuTatMrdizq3JfNeAH";
+$dbname = "295group6";
+
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    // Set PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
